650b7711acae81741b3d627ae3f934f6109f1491
Yang Kimi <kimiyang951116@gmail.com>
2026-05-23 12:01:36 +0800

    refactor: adding more stuff to new folders and files

--- lib/services/geocoding_services.dart
+++ lib/services/geocoding_services.dart
@@ -1,10 +1,59 @@
 import 'dart:convert';
 
+import 'package:geolocator/geolocator.dart' as geo;
 import 'package:http/http.dart' as http;
 import 'package:jio_leh/config/map_env.dart';
 
 class GeoCodingServices {
-  Future<String> getLocationNameFromCoordinates({
+  GeoCodingServices({
+    this.minIntervalSeconds = 20,
+    this.minDistanceMeters = 50,
+  });
+
+  final int minIntervalSeconds;
+  final int minDistanceMeters;
+
+  DateTime? _lastFetchAt;
+  geo.Position? _lastFetchPos;
+
+  bool wouldFetch(geo.Position position) => !_isThrottled(position);
+
+  Future<String?> fetchAreaName(
+    geo.Position position, {
+    bool force = false,
+  }) async {
+    if (!force && _isThrottled(position)) return null;
+
+    final name = await _reverseGeocode(
+      latitude: position.latitude,
+      longitude: position.longitude,
+    );
+
+    _lastFetchAt = DateTime.now();
+    _lastFetchPos = position;
+    return name;
+  }
+
+  bool _isThrottled(geo.Position position) {
+    final lastTime = _lastFetchAt;
+    final lastPos = _lastFetchPos;
+    if (lastTime == null || lastPos == null) return false;
+
+    final recentlyUpdated =
+        DateTime.now().difference(lastTime).inSeconds < minIntervalSeconds;
+
+    final hasNotMovedMuch = geo.Geolocator.distanceBetween(
+          lastPos.latitude,
+          lastPos.longitude,
+          position.latitude,
+          position.longitude,
+        ) <
+        minDistanceMeters;
+
+    return recentlyUpdated && hasNotMovedMuch;
+  }
+
+  Future<String> _reverseGeocode({
     required double latitude,
     required double longitude,
   }) async {
@@ -32,6 +81,26 @@ class GeoCodingServices {
       return 'Unknown location';
     }
 
-    return features.first['place_name'] as String;
+    final firstFeature = features.first as Map<String, dynamic>;
+    final properties =
+        firstFeature['properties'] as Map<String, dynamic>? ?? {};
+    final context = properties['context'] as Map<String, dynamic>? ?? {};
+
+    final name = properties['name'] as String?;
+
+    final neighborhood = context['neighborhood']?['name'] as String?;
+    final locality = context['locality']?['name'] as String?;
+    final district = context['district']?['name'] as String?;
+    final place = context['place']?['name'] as String?;
+    final region = context['region']?['name'] as String?;
+    final country = context['country']?['name'] as String?;
+
+    final area = neighborhood ?? locality ?? district ?? place ?? region ?? name;
+
+    if (area != null && country != null && area != country) {
+      return '$area, $country';
+    }
+
+    return area ?? country ?? 'Unknown location';
   }
 }
