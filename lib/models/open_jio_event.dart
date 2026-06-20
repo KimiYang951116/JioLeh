df0aae1f4f6d01f3e933b331e862ef08916381c1
Justin <justintanjj192@gmail.com>
2026-06-20 18:49:53 +0800

    feat: add dateTime and caption fields to OpenJioEvent model

--- lib/models/open_jio_event.dart
+++ lib/models/open_jio_event.dart
@@ -3,9 +3,13 @@ import 'package:jio_leh/models/user_friend.dart';
 class OpenJioEvent {
   const OpenJioEvent({
     required this.invitedFriends,
+    required this.dateTime, 
+    required this.caption,
   });
 
   final List<UserFriend> invitedFriends;
+  final DateTime dateTime;
+  final String caption;
 
   String get friendNames {
     return invitedFriends
